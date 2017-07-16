47662d47f5b8991b3afea9d9055ed990edc7ec8e
Yan Georget <yan.georget@polytechnique.org>
2017-07-16 23:26:18 +0200

    style

--- scripts/hubot.js
+++ scripts/hubot.js
@@ -3,8 +3,8 @@
 var Bot = require('./src/bot');
 
 module.exports = (robot) => {
-    let bot = new Bot(robot);
-    robot.hear(/.*/, res => {
-        return bot.respond(res);
-    });
+  let bot = new Bot(robot);
+  robot.hear(/.*/, res => {
+    return bot.respond(res);
+  });
 };
