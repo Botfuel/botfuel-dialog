20a1e0617efeba508d2309253a635be07d3c41ee
Yan Georget <yan.georget@polytechnique.org>
2017-09-08 15:23:08 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
-const Brain = require('./brains/brain');
 const DialogManager = require('./dialog_manager');
 const Nlu = require('./nlu');
-const MemoryBrain = require('./brains/memory_brain');
-// const MongoBrain = require('./brains/mongo_brain');
+const MemoryBrain = require('./brain/memory/memory_brain');
 const Bot = require('./bot');
 const Dialog = require('./dialogs/dialog');
 const PromptDialog = require('./dialogs/prompt_dialog');
-const Train = require('./train');
 
 module.exports = {
   Bot,
-  Brain,
   MemoryBrain,
-  // MongoBrain,
   Nlu,
   DialogManager,
   Dialog,
   PromptDialog,
-  Train,
 };
