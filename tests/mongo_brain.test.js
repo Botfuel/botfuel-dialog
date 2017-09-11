574cf508887169dd76489aaf6334a4fbb94e8aba
Kevin Depuydt <kevin@botfuel.io>
2017-09-11 10:49:59 +0200

    add describe method to tests, update MongoBrain tests to delete user created after testing

--- tests/mongo_brain.test.js
+++ tests/mongo_brain.test.js
@@ -1,72 +1,72 @@
+const expect = require('expect.js');
 const MongoBrain = require('../src/brains/mongo/mongo_brain');
+const User = require('../src/brains/mongo/models/user');
 
 const BOT_ID = '1';
+const USER_TEST_ID = 'TEST_USER';
 
-test('add an user', async () => {
-  expect.assertions(1);
-  const brain = new MongoBrain(BOT_ID);
-  const user = await brain.addUser('1');
-  expect(user.userId).toBe('1');
-});
+describe('MongoBrain', () => {
+  after('clean user TEST', async () => {
+    await User.remove({ userId: USER_TEST_ID });
+  });
 
-test('get an user', async () => {
-  expect.assertions(1);
-  const brain = new MongoBrain(BOT_ID);
-  const user = await brain.getUser('1');
-  expect(user.userId).toBe('1');
-});
+  it('test that an user has been added', async () => {
+    const brain = new MongoBrain(BOT_ID);
+    await brain.addUser(USER_TEST_ID);
+    const brainHasUser = await brain.hasUser(USER_TEST_ID);
+    expect(brainHasUser).to.be(true);
+  });
 
-test('set user key', async () => {
-  expect.assertions(1);
-  const brain = new MongoBrain(BOT_ID);
-  await brain.userSet('1', 'name', 'test');
-  const user = await brain.getUser('1');
-  expect(user.name).toBe('test');
-});
+  it('gets an user', async () => {
+    const brain = new MongoBrain(BOT_ID);
+    const user = await brain.getUser(USER_TEST_ID);
+    expect(user.userId).to.be(USER_TEST_ID);
+  });
 
-test('get user key', async () => {
-  expect.assertions(1);
-  const brain = new MongoBrain(BOT_ID);
-  const name = await brain.userGet('1', 'name');
-  expect(name).toBe('test');
-});
+  it('sets user key', async () => {
+    const brain = new MongoBrain(BOT_ID);
+    const user = await brain.userSet(USER_TEST_ID, 'name', 'test');
+    expect(user.name).to.be('test');
+  });
 
-test('push to user key array', async () => {
-  expect.assertions(2);
-  const brain = new MongoBrain(BOT_ID);
-  const dialog = { label: 'travel', entities: { city: 'Paris' } };
-  const user = await brain.userPush('1', 'dialogs', dialog);
-  expect(user.dialogs).toHaveLength(1);
-  expect(user.dialogs[0].label).toEqual('travel');
-});
+  it('gets user value', async () => {
+    const brain = new MongoBrain(BOT_ID);
+    const name = await brain.userGet(USER_TEST_ID, 'name');
+    expect(name).to.be('test');
+  });
 
-test('add conversation to user', async () => {
-  expect.assertions(1);
-  const brain = new MongoBrain(BOT_ID);
-  await brain.addConversation('1');
-  const user = await brain.getUser('1');
-  expect(user.conversations).toHaveLength(1);
-});
+  it('push to user key array', async () => {
+    const brain = new MongoBrain(BOT_ID);
+    const dialog = { label: 'travel', entities: { city: 'Paris' } };
+    const user = await brain.userPush(USER_TEST_ID, 'dialogs', dialog);
+    expect(user.dialogs).to.have.length(1);
+    expect(user.dialogs[0].label).to.be('travel');
+  });
 
-test('get last user conversation', async () => {
-  expect.assertions(1);
-  const brain = new MongoBrain(BOT_ID);
-  await brain.addConversation('1');
-  const conversation = await brain.getLastConversation('1');
-  expect(conversation).toBeDefined();
-});
+  it('add conversation to user', async () => {
+    const brain = new MongoBrain(BOT_ID);
+    await brain.addConversation(USER_TEST_ID);
+    const user = await brain.getUser(USER_TEST_ID);
+    expect(user.conversations).to.have.length(1);
+  });
 
-test('set user last conversation key', async () => {
-  expect.assertions(1);
-  const brain = new MongoBrain(BOT_ID);
-  await brain.addConversation('1');
-  const conversation = await brain.conversationSet('1', 'city', 'Paris');
-  expect(conversation).toHaveProperty('city', 'Paris');
-});
+  it('get last user conversation', async () => {
+    const brain = new MongoBrain(BOT_ID);
+    await brain.addConversation(USER_TEST_ID);
+    const conversation = await brain.getLastConversation(USER_TEST_ID);
+    expect(conversation).not.to.be(null);
+  });
 
-test('get user last conversation key', async () => {
-  expect.assertions(1);
-  const brain = new MongoBrain(BOT_ID);
-  const city = await brain.conversationGet('1', 'city');
-  expect(city).toBe('Paris');
+  it('set user last conversation key', async () => {
+    const brain = new MongoBrain(BOT_ID);
+    await brain.addConversation(USER_TEST_ID);
+    const conversation = await brain.conversationSet(USER_TEST_ID, 'city', 'Paris');
+    expect(conversation).to.have.property('city', 'Paris');
+  });
+
+  it('get user last conversation key', async () => {
+    const brain = new MongoBrain(BOT_ID);
+    const city = await brain.conversationGet(USER_TEST_ID, 'city');
+    expect(city).to.be('Paris');
+  });
 });
