4384ec4778644bb864e153d3cd6bbb38ea3bfd41
Kevin Depuydt <kevin@botfuel.io>
2017-09-12 15:31:59 +0200

    add method to initialize a conversation if last conversation is not valid

--- src/adapters/adapter.js
+++ src/adapters/adapter.js
@@ -6,14 +6,7 @@ class Adapter {
     console.log('Adapter.constructor');
     this.config = config;
     this.bot = bot;
-  }
-
-  async initUserIfNecessary(id) {
-    console.log('Adapter.initUserIfNecessary', id);
-    const userExists = await this.bot.brain.hasUser(id);
-    if (!userExists) {
-      await this.bot.brain.addUser(id);
-    }
+    this.dayInMs = 86400000; // One day in milliseconds
   }
 
   async play(userMessages) {
@@ -24,6 +17,32 @@ class Adapter {
   async run() {
     console.log('Adapter.run');
     throw new Error('Not implemented!');
+  }
+
+  async initUserIfNecessary(id) {
+    console.log('Adapter.initUserIfNecessary', id);
+    const userExists = await this.bot.brain.hasUser(id);
+    if (!userExists) {
+      await this.bot.brain.addUser(id);
+    }
+    await this.initLastConversationIfNecessary(id);
+  }
+
+  async initLastConversationIfNecessary(id) {
+    const lastConversation = await this.bot.brain.getLastConversation(id);
+    console.log('Adapter.initLastConversationIfNecessary', id, lastConversation);
+    if (!this.isLastConversationValid(lastConversation)) {
+      console.log('Adapter.initLastConversationIfNecessary: initialize new');
+      await this.bot.brain.addConversation(id);
+    }
+  }
+
+  isLastConversationValid(conversation) {
+    if (!conversation) {
+      return false;
+    }
+    // return true if last conversation time diff with now is less than one day
+    return (Date.now() - conversation.createdAt) < this.dayInMs;
   }
 }
 
