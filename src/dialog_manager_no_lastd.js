1df0be94196903b5e8a0cc6fb6900bf39360c41a
Kevin Depuydt <kevin@botfuel.io>
2017-09-15 16:14:58 +0200

    add dialog manager without last dialog

---
+++ src/dialog_manager_no_lastd.js
@@ -0,0 +1,118 @@
+const Fs = require('fs');
+const _ = require('underscore');
+
+_.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
+
+/**
+ * Default DialogManager.
+ */
+class DialogManager {
+  /**
+   * Constructor.
+   */
+  constructor(brain, config) {
+    // console.log('DM.constructor');
+    this.brain = brain;
+    this.config = config;
+    this.intentThreshold = this.config.intentThreshold || 0.8;
+  }
+
+  acceptIntent(value) {
+    return (value > this.intentThreshold);
+  }
+
+  /**
+   * Populates and executes the stack.
+   * @param {string} userId the user id
+   * @param {string[]} intents the intents
+   * @param {Object[]} entities the transient entities
+   */
+  async execute(userId, intents, entities) {
+    console.log('DM.execute', userId, intents, entities);
+    const dialogs = await this.brain.userGet(userId, 'dialogs');
+    for (const intent of intents) {
+      if (this.acceptIntent(intent.value)) {
+        const dialogIndex = dialogs.findIndex(d => d.label === intent.label);
+        // if no dialogs with that label is found
+        if (dialogIndex === -1) {
+          await this.next(userId, intent.label, entities);
+        }
+      }
+    }
+    this.responses = [];
+    await this.executeDialogs(userId, entities);
+    return this.responses;
+  }
+
+  /**
+   * Executes the dialogs.
+   * @param {string} userId the user id
+   * @param {Object[]} entities - the entities
+   */
+  async executeDialogs(userId, entities) {
+    console.log('DM.executeDialogs', userId, entities);
+    const dialogs = await this.brain.userGet(userId, 'dialogs');
+    console.log('DM.executeDialogs: dialogs', dialogs);
+    if (dialogs.length > 0) {
+      const currentDialog = Object.assign({}, dialogs[dialogs.length - 1]);
+      const dialogPath = `${this.config.path}/src/controllers/dialogs/${currentDialog.label}`;
+      console.log('DM.executeDialogs: dialogPath', dialogPath);
+      const DialogConstructor = require(dialogPath);
+      const dialog = new DialogConstructor(currentDialog.parameters);
+      const isDone = await dialog.execute(this, userId, entities);
+      if (isDone) {
+        // if dialog is done then pop it from dialogs
+        await this.brain.userPop(userId, 'dialogs');
+        // then continue executing the stack
+        return this.executeDialogs(userId, entities);
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Pushes a dialog on the stack.
+   * @param {string} userId the user id
+   * @param {string} label the dialog label
+   * @param {Object} parameters the dialog parameters
+   */
+  async next(userId, label, parameters) {
+    console.log('DM.next', userId, label, parameters);
+    await this.brain.userPush(userId, 'dialogs', { label, parameters });
+  }
+
+  /**
+   * Says something.
+   * @param {string} userId the user id
+   * @param {string} label the template label
+   * @param {Object} parameters the template parameters
+   */
+  say(userId, label, parameters) {
+    console.log('DM.say', userId, label, parameters);
+    const templatePath = `${this.config.path}/src/views/templates`;
+    const templateName = `${templatePath}/${label}.${this.config.locale}.txt`;
+    console.log('DM.say: templateName', templateName);
+    Fs
+      .readFileSync(templateName, 'utf8')
+      .toString()
+      .split('\n')
+      .forEach((line) => {
+        console.log('DM.say: line', line);
+        const payload = _.template(line)(parameters);
+        console.log('DM.say: payload', payload);
+        if (payload !== '') {
+          const response = {
+            type: 'text',
+            payload,
+            userId,
+            botId: this.config.id,
+            origin: 'bot',
+          };
+          console.log('DM.say: response', response);
+          this.responses.push(response);
+        }
+      });
+  }
+}
+
+module.exports = DialogManager;
