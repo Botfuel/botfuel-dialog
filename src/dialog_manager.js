cff50f98abd9a7f6e07a6efff4ce3aeb7e9feb35
Kevin Depuydt <kevin@botfuel.io>
2017-09-15 10:54:09 +0200

    small fixes

--- src/dialog_manager.js
+++ src/dialog_manager.js
@@ -11,7 +11,7 @@ class DialogManager {
    * Constructor.
    */
   constructor(brain, config) {
-    // console.log('DialogManager.constructor');
+    // console.log('DM.constructor');
     this.brain = brain;
     this.config = config;
     this.intentThreshold = this.config.intentThreshold || 0.8;
@@ -28,29 +28,27 @@ class DialogManager {
    * @param {Object[]} entities the transient entities
    */
   async execute(userId, intents, entities) {
-    console.log('DialogManager.execute', userId, intents, entities);
+    console.log('DM.execute', userId, intents, entities);
     const dialogsBefore = await this.brain.userGet(userId, 'dialogs');
-    console.log('DialogManager.execute: dialogsBefore', dialogsBefore);
+    console.log('DM.execute: dialogsBefore', dialogsBefore);
     const lastDialogBefore = await this.brain.userGet(userId, 'lastDialog');
-    console.log('DialogManager.execute: lastDialogBefore', lastDialogBefore);
+    console.log('DM.execute: lastDialogBefore', lastDialogBefore);
     for (const intent of intents) {
       if (this.acceptIntent(intent.value)) {
-        await this.next(userId, intent.label, entities);
+        await this.addDialog(userId, intent.label, entities);
       }
     }
     const dialogs = await this.brain.userGet(userId, 'dialogs');
-    console.log('DialogManager.execute: dialogs', dialogs);
-    const lastDialogAfter = await this.brain.userGet(userId, 'lastDialog');
-    console.log('DialogManager.execute: lastDialogAfter', lastDialogAfter);
     if (dialogs.length > 0) {
       const lastDialog = await this.brain.userGet(userId, 'lastDialog');
       if (Object.hasOwnProperty.call(lastDialog, 'label')) {
-        console.log('DialogManager push last dialog to dialogs', lastDialog);
+        console.log('DM.execute -> push last dialog to dialogs', lastDialog);
         await this.brain.userPush(userId, 'dialogs', lastDialog);
       }
     }
     this.responses = [];
     await this.executeDialogs(userId, entities);
+    console.log('DM.execute: responses', this.responses);
     return this.responses;
   }
 
@@ -60,18 +58,15 @@ class DialogManager {
    * @param {Object[]} entities - the entities
    */
   async executeDialogs(userId, entities) {
-    console.log('DialogManager.executeDialogs', userId, entities);
+    console.log('DM.executeDialogs', userId, entities);
     const dialogs = await this.brain.userGet(userId, 'dialogs');
-    console.log('DialogManager.executeDialogs: dialogs', dialogs);
-    const lastDialog = await this.brain.userGet(userId, 'lastDialog');
-    console.log('DialogManager.executeDialogs: lastDialog', lastDialog);
     if (dialogs.length > 0) {
       const dialogData = await this.brain.userShift(userId, 'dialogs');
-      console.log('DM.executeDialogs set new last dialog', dialogData);
+      console.log('DM.executeDialogs -> set new last dialog', dialogData);
       await this.brain.userSet(userId, 'lastDialog', dialogData);
-      console.log('DialogManager.executeDialogs: dialogData', dialogData);
+      console.log('DM.executeDialogs: dialogData', dialogData);
       const dialogPath = `${this.config.path}/src/controllers/dialogs/${dialogData.label}`;
-      console.log('DialogManager.executeDialogs: dialogPath', dialogPath);
+      console.log('DM.executeDialogs: dialogPath', dialogPath);
       const DialogConstructor = require(dialogPath);
       const dialog = new DialogConstructor(dialogData.parameters);
       const run = await dialog.execute(this, userId, entities);
@@ -79,6 +74,7 @@ class DialogManager {
         return this.executeDialogs(userId, entities);
       }
     }
+    return true;
   }
 
   /**
@@ -87,8 +83,8 @@ class DialogManager {
    * @param {string} label the dialog label
    * @param {Object} parameters the dialog parameters
    */
-  async next(userId, label, parameters) {
-    console.log('DialogManager.next', userId, label, parameters);
+  async addDialog(userId, label, parameters) {
+    console.log('DM.addDialog', userId, label, parameters);
     await this.brain.userPush(userId, 'dialogs', { label, parameters });
   }
 
@@ -99,28 +95,29 @@ class DialogManager {
    * @param {Object} parameters the template parameters
    */
   say(userId, label, parameters) {
-    console.log('DialogManager.say', userId, label, parameters);
+    console.log('DM.say', userId, label, parameters);
     const templatePath = `${this.config.path}/src/views/templates/`;
     const templateName = `${templatePath}/${label}.${this.config.locale}.txt`;
-    console.log('DialogManager.say: templateName', templateName);
+    console.log('DM.say: templateName', templateName);
     Fs
       .readFileSync(templateName, 'utf8')
       .toString()
       .split('\n')
       .forEach((line) => {
-        console.log('DialogManager.say: line', line);
-        const payload = _.template(line)(parameters);
-        console.log('DialogManager.say: payload', payload);
-        if (payload !== '') {
-          const response = {
-            type: 'text',
-            userId,
-            botId: this.config.id,
-            origin: 'bot',
-            payload,
-          };
-          console.log('DialogManager.say: response', response);
-          this.responses.push(response);
+        if (line.length > 0) {
+          console.log('DM.say: line', line);
+          const payload = _.template(line)(parameters);
+          console.log('DM.say: payload', payload);
+          if (payload !== '') {
+            const response = {
+              type: 'text',
+              userId,
+              botId: this.config.id,
+              origin: 'bot',
+              payload,
+            };
+            this.responses.push(response);
+          }
         }
       });
   }
