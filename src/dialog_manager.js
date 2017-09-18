80ff30348ecf954f33946c9eaa6ab2193a0d0017
Kevin Depuydt <kevin@botfuel.io>
2017-09-18 13:49:19 +0200

    small fixes

--- src/dialog_manager.js
+++ src/dialog_manager.js
@@ -47,7 +47,7 @@ class DialogManager {
     const dialogs = await this.brain.userGet(userId, 'dialogs');
     for (const intent of intents) {
       if (this.acceptIntent(intent.value)) {
-        if (dialogs.length == 0 || dialogs[dialogs.length - 1].label !== intent.label) {
+        if (dialogs.length === 0 || dialogs[dialogs.length - 1].label !== intent.label) {
           dialogs.push({ label: intent.label, parameters: entities });
         }
       }
