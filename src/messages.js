c723d1745e45ddcdb73f53b601186bf766609fb5
Yan Georget <yan.georget@polytechnique.org>
2017-09-19 18:08:15 +0200

    refactoring

--- src/messages.js
+++ src/messages.js
@@ -10,6 +10,21 @@ class Messages {
     };
   }
 
+  static getBotImageMessage(botId, userId, payload) {
+    console.log('Messages.getBotImageMessage', botId, userId, payload);
+    throw new Error('Not yet implemented!');
+  }
+
+  static getBotPostbackButtonsMessage(botId, userId, payload) {
+    console.log('Messages.getBotPostbackButtonsMessage', botId, userId, payload);
+    throw new Error('Not yet implemented!');
+  }
+
+  static getBotReplyButtonsMessage(botId, userId, payload) {
+    console.log('Messages.getBotReplyButtonsMessage', botId, userId, payload);
+    throw new Error('Not yet implemented!');
+  }
+
   static getUserTextMessage(botId, userId, payload) {
     console.log('Messages.getUserTextMessage', botId, userId, payload);
     return {
@@ -21,6 +36,15 @@ class Messages {
     };
   }
 
+  static getUserImageMessage(botId, userId, payload) {
+    console.log('Messages.getUserImageMessage', botId, userId, payload);
+    throw new Error('Not yet implemented!');
+  }
+
+  static getUserPostbackMessage(botId, userId, payload) {
+    console.log('Messages.getUserPostbackMessage', botId, userId, payload);
+    throw new Error('Not yet implemented!');
+  }
 }
 
 module.exports = Messages;
