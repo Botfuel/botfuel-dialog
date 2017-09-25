f01733176eb0a88b129b47758448c58cf0b20f66
Kevin Depuydt <kevin@botfuel.io>
2017-09-25 18:38:55 +0200

    update messages type

--- src/messages.js
+++ src/messages.js
@@ -9,6 +9,23 @@ class Messages {
     return Messages.create(Messages.TYPE_ACTIONS, Messages.SENDER_BOT, bot, user, value, options);
   }
 
+  static botQuickReplies(bot, user, value, options) {
+    console.log('Messages.botQuickReplies', bot, user, value, options);
+    return Messages.create(
+      Messages.TYPE_QUICK_REPLIES,
+      Messages.SENDER_BOT,
+      bot,
+      user,
+      value,
+      options,
+    );
+  }
+
+  static botCards(bot, user, value, options) {
+    console.log('Messages.botCards', bot, user, value, options);
+    return Messages.create(Messages.TYPE_CARDS, Messages.SENDER_BOT, bot, user, value, options);
+  }
+
   static userText(bot, user, value, options) {
     console.log('Messages.userText', bot, user, value, options);
     return Messages.create(Messages.TYPE_TEXT, Messages.SENDER_USER, bot, user, value, options);
@@ -42,7 +59,9 @@ class Messages {
 Messages.TYPE_TEXT = 'text';
 Messages.TYPE_TABLE = 'table';
 Messages.TYPE_ACTIONS = 'actions';
+Messages.TYPE_QUICK_REPLIES = 'quick_replies';
 Messages.TYPE_POSTBACK = 'postback';
+Messages.TYPE_CARDS = 'cards';
 Messages.SENDER_BOT = 'bot';
 Messages.SENDER_USER = 'user';
 
