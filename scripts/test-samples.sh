71d7814d685b3e5c3e4961a9aee4fdf2908b739f
Kevin Depuydt <kevin@botfuel.io>
2017-10-02 11:21:57 +0200

    update test-samples script

--- scripts/test-samples.sh
+++ scripts/test-samples.sh
@@ -15,18 +15,20 @@ fi
 # delete samples dir if exists
 if [ -d ${SAMPLES_DIR} ]
 then
-  echo "delete $SAMPLES_DIR directory"
   rm -rf ${SAMPLES_DIR}
 fi
 # create samples dir
 mkdir ${SAMPLES_DIR}
+echo "$SAMPLES_DIR has been cleaned"
 
-# create logs dir if not exists
-if ! [ -d ${LOGS_DIR} ]
+# delete logs dir if exists
+if [ -d ${LOGS_DIR} ]
 then
-  echo "create $LOGS_DIR directory"
-  mkdir ${LOGS_DIR}
+  rm -rf ${LOGS_DIR}
 fi
+# create logs dir
+mkdir ${LOGS_DIR}
+echo "$LOGS_DIR has been cleaned"
 
 # loop over SAMPLE.txt to extract samples list
 while IFS= read -r repository
@@ -50,7 +52,9 @@ do
     # install dependencies
     npm install
     # test and log
-    npm run test > ../../${LOGS_DIR}/${sample_name}.log
+    log_file_path=../../${LOGS_DIR}/${sample_name}.log
+    date > ${log_file_path}
+    npm run test >> ${log_file_path}
     # go back to samples dir
     cd ..
 done
