4d5cdba494b5a6b1a750b1fd913005b154024af3
Kevin Depuydt <kevin@botfuel.io>
2017-10-04 17:35:50 +0200

    rework test-samples script

--- scripts/test-samples.sh
+++ scripts/test-samples.sh
@@ -4,53 +4,87 @@ LOGS_DIR='logs'
 SAMPLES_DIR='samples'
 SAMPLES_LIST_FILE='SAMPLES.txt'
 SAMPLES=()
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+BLANK='\033[0m'
 
-exec_command() {
-  echo "Running "$*
-  eval $* & PID=$!
-  printf "["
-  # While process is running...
+screen_output() {
+  MESSAGE=$1
+  PID=$2
+  time=0
   while kill -0 $PID 2> /dev/null; do
-    printf  "."
+    echo -ne "${ORANGE}${MESSAGE} (${time}s) \r"
+    time=$((time + 1))
     sleep 1
   done
-  printf "]\n"
+  TASK_TIME=$((TASK_TIME + time))
+  echo -e "${GREEN}${MESSAGE} (${time}s) OK!    "
 }
 
-test_sample () {
-  # get sample from arguments
+clone() {
   SAMPLE=$1
-  # clone repository
-  GIT_COMMAND="git clone ${SAMPLE} &> /dev/null"
-  exec_command $GIT_COMMAND
-  # get sample name
-  SAMPLE_NAME=$(echo ${SAMPLE} | cut -d'/' -f2)
-  # move into sample name
+  git clone ${SAMPLE} &> /dev/null & PID=$!
+  screen_output "Clone sample repository" ${PID}
+}
+
+install() {
+  SAMPLE_NAME=$1
   cd ${SAMPLE_NAME}
-  # install dependencies
-  NPM_INSTALL_COMMAND="npm install &> /dev/null"
-  exec_command $NPM_INSTALL_COMMAND
-  # link sample to local bot-sdk2
-  NPM_LINK_COMMAND="npm link ../../ &> /dev/null"
-  exec_command $NPM_LINK_COMMAND
-  # train bot model
-  NPM_TRAIN_COMMAND="npm run train -- shell_config &> /dev/null"
-  exec_command $NPM_TRAIN_COMMAND
-  # run tests and log
+  npm install &> /dev/null & PID=$!
+  screen_output "Install dependencies" ${PID}
+}
+
+link() {
+  npm link ../../ &> /dev/null & PID=$!
+  screen_output "Link to local sdk" ${PID}
+}
+
+train() {
+  npm run train -- shell_config &> /dev/null & PID=$!
+  screen_output "Train sample" ${PID}
+}
+
+test_and_log() {
+  SAMPLE_NAME=$1
   log_file_path=../../${LOGS_DIR}/${SAMPLE_NAME}.log
   ok_file_path=../../${LOGS_DIR}/${SAMPLE_NAME}.ok
   ko_file_path=../../${LOGS_DIR}/${SAMPLE_NAME}.ko
   date > ${log_file_path}
-  NPM_TEST_COMMAND="npm run test >> ${log_file_path} && touch ${ok_file_path} || touch ${ko_file_path}"
-  exec_command $NPM_TEST_COMMAND
+
+  npm run test >> ${log_file_path} && touch ${ok_file_path} || touch ${ko_file_path} &> /dev/null & PID=$!
+  screen_output "Test sample" ${PID}
+
   if [ -f ${ok_file_path} ]
   then
-    echo "OK"
-    exit
+    echo -e "${GREEN}Tests passed !${BLANK}"
   else
-    echo "KO"
-    exit
+    echo -e "${RED}Tests failed !${BLANK}"
   fi
+}
+
+test_sample() {
+  TASK_TIME=0
+  # get sample from arguments
+  SAMPLE=$1
+  # get sample name
+  SAMPLE_NAME=$(echo ${SAMPLE} | cut -d'/' -f2)
+  echo "** $SAMPLE_NAME **" | tr /a-z/ /A-Z/
+  # clone repository
+  clone ${SAMPLE}
+  # install dependencies
+  install ${SAMPLE_NAME}
+  # link sample to local bot-sdk2
+  link
+  # train bot model
+  train
+  # run tests and log
+  test_and_log ${SAMPLE_NAME}
+  # print task time
+  echo -e "\n${BLUE}Total time: ${TASK_TIME}s${BLANK}\n"
+  # exit sub shell
+  exit
 }
 
 # check if samples.txt exists
@@ -67,7 +101,6 @@ then
 fi
 # create samples dir
 mkdir ${SAMPLES_DIR}
-echo "Directory $SAMPLES_DIR has been cleaned"
 
 # delete logs dir if exists
 if [ -d ${LOGS_DIR} ]
@@ -76,7 +109,6 @@ then
 fi
 # create logs dir
 mkdir ${LOGS_DIR}
-echo "Directory $LOGS_DIR has been cleaned"
 
 # loop over SAMPLE.txt to extract samples list
 while IFS= read -r repository
