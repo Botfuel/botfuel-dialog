3151e13fb97e17fe17bd9eedc0d8088ab412bbe4
Yan Georget <yan@botfuel.io>
2017-10-04 12:46:18 +0200

    style

--- scripts/test-samples.sh
+++ scripts/test-samples.sh
@@ -5,6 +5,44 @@ SAMPLES_DIR='samples'
 SAMPLES_LIST_FILE='SAMPLES.txt'
 SAMPLES=()
 
+exec_command() {
+  echo "Running "$*
+  eval $* & PID=$!
+  printf "["
+  # While process is running...
+  while kill -0 $PID 2> /dev/null; do
+    printf  "."
+    sleep 1
+  done
+  printf "]\n"
+}
+
+test_sample () {
+  # get sample from arguments
+  SAMPLE=$1
+  # clone repository
+  GIT_COMMAND="git clone ${SAMPLE} &> /dev/null"
+  exec_command $GIT_COMMAND
+  # get sample name
+  SAMPLE_NAME=$(echo ${SAMPLE} | cut -d'/' -f2)
+  # move into sample name
+  cd ${SAMPLE_NAME}
+  # install dependencies
+  NPM_INSTALL_COMMAND="npm install &> /dev/null"
+  exec_command $NPM_INSTALL_COMMAND
+  # link sample to local bot-sdk2
+  NPM_LINK_COMMAND="npm link ../../ &> /dev/null"
+  exec_command $NPM_LINK_COMMAND
+  # train bot model
+  NPM_TRAIN_COMMAND="npm run train -- shell_config &> /dev/null"
+  exec_command $NPM_TRAIN_COMMAND
+  # run tests and log
+  log_file_path=../../${LOGS_DIR}/${SAMPLE_NAME}.log
+  date > ${log_file_path}
+  NPM_TEST_COMMAND="npm run test >> ${log_file_path} && echo 'OK' || echo 'KO'"
+  exec_command $NPM_TEST_COMMAND
+}
+
 # check if samples.txt exists
 if ! [ -f ${SAMPLES_LIST_FILE} ]
 then
@@ -36,47 +74,8 @@ do
   SAMPLES+=(${repository}) # Append line to the array
 done < ${SAMPLES_LIST_FILE}
 
-test_sample () {
-  # go into samples dir
-  cd ${SAMPLES_DIR}
-  # get sample from arguments
-  sample=$1
-  # get sample name
-  sample_name=$(echo ${sample} | cut -d'/' -f2)
-  # clone repository
-  GIT_COMMAND="git clone ${sample} &> /dev/null"
-  exec_command $GIT_COMMAND
-  # move into sample name
-  echo "Move into $sample_name"
-  cd ${sample_name}
-  # install dependencies
-  NPM_INSTALL_COMMAND="npm install &> /dev/null"
-  exec_command $NPM_INSTALL_COMMAND
-  # link sample to local bot-sdk2
-  NPM_LINK_COMMAND="npm link ../../ &> /dev/null"
-  exec_command $NPM_LINK_COMMAND
-  # train bot model
-  NPM_TRAIN_COMMAND="npm run train -- shell_config &> /dev/null"
-  exec_command $NPM_TRAIN_COMMAND
-  # run tests and log
-  log_file_path=../../${LOGS_DIR}/${sample_name}.log
-  date > ${log_file_path}
-  NPM_TEST_COMMAND="npm run test >> ${log_file_path} && echo 'OK' || echo 'KO'"
-  exec_command $NPM_TEST_COMMAND
-}
-
-exec_command() {
-  echo "Running "$*
-  eval $* & PID=$!
-  printf "["
-  # While process is running...
-  while kill -0 $PID 2> /dev/null; do
-    printf  "."
-    sleep 1
-  done
-  printf "]\n"
-}
-
+# go into samples dir
+cd ${SAMPLES_DIR}
 # for each repo
 for sample in ${SAMPLES[@]}
 do
