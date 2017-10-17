a9e96a13d2380ed621260799de0d644eff1b06fe
Kevin Depuydt <kevin@botfuel.io>
2017-10-17 12:34:59 +0200

    look for template in sdk

--- src/template_manager.js
+++ src/template_manager.js
@@ -1,4 +1,4 @@
-const Fs = require('fs');
+const fs = require('fs');
 const _ = require('underscore');
 const BotTextMessage = require('./views/parts/bot_text_message');
 
@@ -6,10 +6,26 @@ _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
 
 class TemplateManager {
   constructor(config, namespace) {
-    this.templatePath = `${config.path}/src/views/templates`;
+    this.templatePath = `${config.path}/views/templates`;
     this.botId = config.id;
     this.locale = config.locale;
     this.namespace = namespace;
+  }
+
+  getTemplatePath(template) {
+    console.log('TemplateManager.getTemplatePath');
+    const paths = [
+      `${this.templatePath}/${template}.${this.locale}.txt`,
+      `${__dirname}/views/templates/${template}.${this.locale}.txt`,
+    ];
+    for (const path of paths) {
+      console.log('TemplateManager.getTemplatePath: path exists ?', path, fs.existsSync(path));
+      if (fs.existsSync(path)) {
+        console.log('TemplateManager.getTemplatePath: path exists', path);
+        return path;
+      }
+    }
+    return null;
   }
 
   /**
@@ -21,14 +37,17 @@ class TemplateManager {
   compile(userId, template, parameters) {
     console.log('TemplateManager.compile', userId, template, parameters);
     // TODO: resolve the template given the label (allowing fallback)
-    const templateName = `${this.templatePath}/${template}.${this.locale}.txt`;
-    return Fs
-      .readFileSync(templateName, 'utf8')
-      .toString()
-      .split('\n')
-      .map(line => _.template(line)(parameters))
-      .filter(Boolean)
-      .map(text => new BotTextMessage(this.botId, userId, text).toJson());
+    const templatePath = this.getTemplatePath(template);
+    if (templatePath) {
+      return fs
+        .readFileSync(templatePath, 'utf8')
+        .toString()
+        .split('\n')
+        .map(line => _.template(line)(parameters))
+        .filter(Boolean)
+        .map(text => new BotTextMessage(this.botId, userId, text).toJson());
+    }
+    return null;
   }
 
   entityAsk(id, entity) {
