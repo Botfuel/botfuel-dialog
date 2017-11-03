5bfd39bb49b6287c1be69039d20bf7660bc0f2bf
Kevin Depuydt <kevin@botfuel.io>
2017-11-03 16:45:12 +0100

    don't use a bot when not necessary in tests

--- tests/dialog_manager/dialog_manager.test.js
+++ tests/dialog_manager/dialog_manager.test.js
@@ -1,48 +1,53 @@
 /* eslint-disable prefer-arrow-callback */
 
 const expect = require('expect.js');
-const Bot = require('../../src/bot');
+const DialogManager = require('../../src/dialog_manager');
+const MemoryBrain = require('../../src/brains/memory_brain');
+const TestAdapter = require('../../src/adapters/test_adapter');
 const { BotTextMessage } = require('../../src/messages');
 
 const TEST_USER = '1';
+const TEST_BOT = process.env.BOT_ID;
 
 // require('../../src/logger_manager').configure({ logger: 'botfuel'});
 
 describe('DialogManager', function () {
-  const bot = new Bot({ path: __dirname, locale: 'en', adapter: 'test' });
+  const brain = new MemoryBrain(TEST_BOT);
+  const dm = new DialogManager(brain, { path: __dirname, locale: 'en' });
 
   beforeEach(async function () {
-    await bot.brain.clean();
-    await bot.brain.initUserIfNecessary(TEST_USER);
+    await brain.clean();
+    await brain.initUserIfNecessary(TEST_USER);
   });
 
   it('when given a label, it should return the correct path', function () {
-    expect(bot.dm.getDialogPath('test_dialog'))
+    expect(dm.getDialogPath('test_dialog'))
       .to
       .eql(`${__dirname}/src/dialogs/test_dialog`);
   });
 
   it('when given an unknown label, it should return null', function () {
-    expect(bot.dm.getDialogPath('unknown_dialog')).to.be(null);
+    expect(dm.getDialogPath('unknown_dialog')).to.be(null);
   });
 
   it('should not crash when no intent', async function () {
-    await bot.dm.execute(bot.adapter, TEST_USER, [], []);
-    expect(bot.adapter.log).to.eql([
-      new BotTextMessage('Not understood.').toJson(bot.id, TEST_USER),
+    const adapter = new TestAdapter({ id: TEST_BOT }, {});
+    await dm.execute(adapter, TEST_USER, [], []);
+    expect(adapter.log).to.eql([
+      new BotTextMessage('Not understood.').toJson(TEST_BOT, TEST_USER),
     ]);
   });
 
   it('should keep on the stack a dialog which is waiting', async function () {
-    await bot.dm.execute(null, TEST_USER, [{ label: 'waiting_dialog', value: 1.0 }], []);
-    const user = await bot.dm.brain.getUser(TEST_USER);
+    await dm.execute(null, TEST_USER, [{ label: 'waiting_dialog', value: 1.0 }], []);
+    const user = await dm.brain.getUser(TEST_USER);
     expect(user.dialogs.length).to.be(1);
   });
 
   it('should not stack the same dialog twice', async function () {
-    await bot.dm.execute(null, TEST_USER, [{ label: 'waiting_dialog', value: 1.0 }], []);
-    await bot.dm.execute(null, TEST_USER, [{ label: 'waiting_dialog', value: 1.0 }], []);
-    const user = await bot.dm.brain.getUser(TEST_USER);
+    await dm.execute(null, TEST_USER, [{ label: 'waiting_dialog', value: 1.0 }], []);
+    await dm.execute(null, TEST_USER, [{ label: 'waiting_dialog', value: 1.0 }], []);
+    const user = await dm.brain.getUser(TEST_USER);
     expect(user.dialogs.length).to.be(1);
   });
 });
--- tests/dialogs/prompt_dialog.test.js
+++ tests/dialogs/prompt_dialog.test.js
@@ -1,75 +1,76 @@
 /* eslint-disable prefer-arrow-callback */
 
 const expect = require('expect.js');
-const Bot = require('../../src/bot');
 const PromptDialog = require('../../src/dialogs/prompt_dialog');
+const MemoryBrain = require('../../src/brains/memory_brain');
+const TestAdapter = require('../../src/adapters/test_adapter');
 const { BotTextMessage } = require('../../src/messages');
 
 const TEST_USER = '1';
+const TEST_BOT = process.env.BOT_ID;
 
 // require('../../src/logger_manager').configure({ logger: 'botfuel'});
 
 describe('PromptDialog', function () {
-  const bot = new Bot({ path: __dirname, locale: 'en', adapter: 'test' });
+  const brain = new MemoryBrain(TEST_BOT);
   const prompt = new PromptDialog(
     { path: __dirname, locale: 'en' },
-    bot.brain,
+    brain,
     { namespace: 'testdialog', entities: { dim1: null, dim2: null } },
   );
 
   beforeEach(async function () {
-    await bot.brain.clean();
-    await bot.brain.initUserIfNecessary(TEST_USER);
-  });
-
-  afterEach(function () {
-    bot.adapter.log = [];
+    await brain.clean();
+    await brain.initUserIfNecessary(TEST_USER);
   });
 
   it('when given no entity, should list both and ask for one', async function () {
+    const adapter = new TestAdapter({ id: TEST_BOT }, {});
     await prompt.execute(
-      bot.adapter,
+      adapter,
       TEST_USER,
       [],
     );
-    expect(bot.adapter.log).to.eql([
-      new BotTextMessage('Entities needed: dim1, dim2').toJson(bot.id, TEST_USER),
-      new BotTextMessage('Which dim1?').toJson(bot.id, TEST_USER),
+    expect(adapter.log).to.eql([
+      new BotTextMessage('Entities needed: dim1, dim2').toJson(TEST_BOT, TEST_USER),
+      new BotTextMessage('Which dim1?').toJson(TEST_BOT, TEST_USER),
     ]);
-    const user = await bot.brain.getUser(TEST_USER);
+    const user = await brain.getUser(TEST_USER);
     expect(user.conversations.length).to.be(1);
     expect(user.conversations[0].testdialog.dim1).to.be(undefined);
     expect(user.conversations[0].testdialog.dim2).to.be(undefined);
   });
 
   it('when given a first entity, should list both and ask for the second one', async function () {
+    const adapter = new TestAdapter({ id: TEST_BOT }, {});
     await prompt.execute(
-      bot.adapter,
+      adapter,
       TEST_USER,
       [{ dim: 'dim1', body: 'dim1' }],
     );
-    expect(bot.adapter.log).to.eql([
-      new BotTextMessage('Entities defined: dim1').toJson(bot.id, TEST_USER),
-      new BotTextMessage('Entities needed: dim2').toJson(bot.id, TEST_USER),
-      new BotTextMessage('Which dim2?').toJson(bot.id, TEST_USER),
+    expect(adapter.log).to.eql([
+      new BotTextMessage('Entities defined: dim1').toJson(TEST_BOT, TEST_USER),
+      new BotTextMessage('Entities needed: dim2').toJson(TEST_BOT, TEST_USER),
+      new BotTextMessage('Which dim2?').toJson(TEST_BOT, TEST_USER),
     ]);
-    const user = await bot.brain.getUser(TEST_USER);
+    const user = await brain.getUser(TEST_USER);
     expect(user.conversations.length).to.be(1);
     expect(user.conversations[0].testdialog.dim1.dim).to.be('dim1');
     expect(user.conversations[0].testdialog.dim2).to.be(undefined);
   });
 
   it('when given both entity, should ask none', async function () {
+    const adapter = new TestAdapter({ id: TEST_BOT }, {});
     await prompt.execute(
-      bot.adapter,
+      adapter,
       TEST_USER,
       [{ dim: 'dim1', body: 'dim1' }, { dim: 'dim2', body: 'dim2' }],
       PromptDialog.STATUS_READY,
     );
-    expect(bot.adapter.log).to.eql([
-      new BotTextMessage('Entities defined: dim1, dim2').toJson(bot.id, TEST_USER),
+    expect(adapter.log).to.eql([
+      new BotTextMessage('Entities defined: dim1, dim2').toJson(TEST_BOT, TEST_USER),
     ]);
-    const user = await bot.brain.getUser(TEST_USER);
+    const user = await brain.getUser(TEST_USER);
     expect(user.conversations.length).to.be(1);
     expect(user.conversations[0].testdialog.dim1.dim).to.be('dim1');
     expect(user.conversations[0].testdialog.dim2.dim).to.be('dim2');
