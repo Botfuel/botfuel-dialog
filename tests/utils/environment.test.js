4b8b2a8266b0c00e29316cb62100821fdc965bd3
Kevin Depuydt <kevin@botfuel.io>
2018-01-11 11:05:30 +0100

    fix tests

--- tests/utils/environment.test.js
+++ tests/utils/environment.test.js
@@ -14,13 +14,64 @@
  * limitations under the License.
  */
 
-const { checkEnvironmentVariables } = require('../../src/utils/environment');
-const SdkError = require('../../src/errors/sdk-error');
+const { checkCredentials } = require('../../src/utils/environment');
+const { defaultConfig } = require('../../src/config');
+const MissingCredentialsError = require('../../src/errors/missing-credentials-error');
+
+const buildConfig = configPart => Object.assign({}, defaultConfig, configPart);
 
 describe('Environment utils', () => {
-  test('should throw an error when BOTFUEL_APP_TOKEN is not defined', async () => {
-    // delete BOTFUEL_APP_TOKEN to test that the function throw an error
-    delete process.env.BOTFUEL_APP_TOKEN;
-    expect(checkEnvironmentVariables).toThrowError(SdkError);
+  describe('When BOTFUEL_APP_TOKEN is not defined', () => {
+    beforeAll(() => {
+      delete process.env.BOTFUEL_APP_TOKEN;
+    });
+
+    test('should throw an error when using botfuel adapter', async () => {
+      const config = buildConfig({ adapter: 'botfuel' });
+      const f = () => checkCredentials(config);
+      expect(f).toThrowError(MissingCredentialsError);
+    });
+
+    test('should throw an error when using mongo brain', async () => {
+      const config = buildConfig({ brain: 'mongo' });
+      const f = () => checkCredentials(config);
+      expect(f).toThrowError(MissingCredentialsError);
+    });
+  });
+
+  describe('When BOTFUEL_APP_ID is not defined', () => {
+    beforeAll(() => {
+      delete process.env.BOTFUEL_APP_ID;
+    });
+
+    test('should throw an error when using botfuel QnA', async () => {
+      const config = buildConfig({ qna: true });
+      const f = () => checkCredentials(config);
+      expect(f).toThrowError(MissingCredentialsError);
+    });
+
+    test('should throw an error when using spellchecking service', async () => {
+      const config = buildConfig({ spellchecking: true });
+      const f = () => checkCredentials(config);
+      expect(f).toThrowError(MissingCredentialsError);
+    });
+  });
+
+  describe('When BOTFUEL_APP_KEY is not defined', () => {
+    beforeAll(() => {
+      delete process.env.BOTFUEL_APP_KEY;
+    });
+
+    test('should throw an error when using botfuel QnA', async () => {
+      const config = buildConfig({ qna: true });
+      const f = () => checkCredentials(config);
+      expect(f).toThrowError(MissingCredentialsError);
+    });
+
+    test('should throw an error when using spellchecking service', async () => {
+      const config = buildConfig({ spellchecking: true });
+      const f = () => checkCredentials(config);
+      expect(f).toThrowError(MissingCredentialsError);
+    });
   });
 });
