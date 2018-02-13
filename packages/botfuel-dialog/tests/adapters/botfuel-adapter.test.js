33b07698834908b56172cadcf37d18c6e6d22336
Kevin Depuydt <kevin@botfuel.io>
2018-02-13 11:03:45 +0100

    add missing files

---
+++ packages/botfuel-dialog/tests/adapters/botfuel-adapter.test.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2017 - present, Botfuel (https://www.botfuel.io).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable quotes */
+
+const BotfuelAdapter = require('../../src/adapters/botfuel-adapter');
+const BotTextMessage = require('../../src/messages/bot-text-message');
+
+describe('BotfuelAdapter', () => {
+  test('should add properties to the json message', async () => {
+    const message = new BotTextMessage('message');
+    const extended = new BotfuelAdapter({}).addProperties(message.toJson('USER'));
+    expect(Object.keys(extended)).toEqual([
+      'id',
+      'adapter',
+      'timestamp',
+      'type',
+      'sender',
+      'user',
+      'payload',
+    ]);
+    expect(extended).toHaveProperty('user', 'USER');
+    expect(extended).toHaveProperty('payload.value', 'message');
+    expect(extended).toHaveProperty('adapter', 'botfuel');
+  });
+});
---
+++ packages/botfuel-dialog/tests/adapters/test-adapter.test.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2017 - present, Botfuel (https://www.botfuel.io).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable quotes */
+
+const TestAdapter = require('../../src/adapters/test-adapter');
+const BotTextMessage = require('../../src/messages/bot-text-message');
+
+describe('TestAdapter', () => {
+  test('should add properties to the json message', async () => {
+    const message = new BotTextMessage('message');
+    const extended = new TestAdapter({}).addProperties(message.toJson('USER'));
+    expect(Object.keys(extended)).toEqual(['type', 'sender', 'user', 'payload']);
+    expect(extended).not.toHaveProperty('id');
+    expect(extended).not.toHaveProperty('adapter');
+    expect(extended).not.toHaveProperty('timestamp');
+  });
+});
