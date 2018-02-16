707f423d735a3caadce35e099e5a69b96cfa5c96
Kevin Depuydt <kevin@botfuel.io>
2018-02-16 16:05:42 +0100

    add tests for loggers

---
+++ packages/botfuel-dialog/tests/loggers/src/loggers/test-logger.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2017 - present, Botfuel (https://www.botfuel.io).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const WinstonWrapper = require('logtown-winston');
+const winston = require('winston');
+
+const transports = [
+  new winston.transports.Console({
+    json: false,
+    colorize: true,
+    prettyPrint: true,
+    timestamp: true,
+    handleExceptions: true,
+    align: false,
+    level: 'debug',
+  }),
+];
+
+const options = {
+  exitOnError: false,
+};
+
+module.exports = {
+  wrapper: new WinstonWrapper(transports, options),
+  config: {},
+};
