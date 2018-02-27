44fc9c23458f9a633d36149f59df06038abe1f24
Michel Blancard <michel@botfuel.io>
2018-02-27 16:35:14 +0100

    feat: Add resolver for NLU modules

---
+++ packages/botfuel-dialog/src/nlu-resolver.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2017 - present, Botfuel (https://www.botfuel.io).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const logger = require('logtown')('NluResolver');
+const Resolver = require('./resolver');
+
+/**
+ * The adapter resolver resolves the adapter at startup.
+ */
+class NluResolver extends Resolver {
+  /**
+   * @constructor
+   * @param {Object} bot - the bot
+   */
+  constructor(bot) {
+    super(bot.config, 'nlu');
+    this.bot = bot;
+  }
+
+  /** @inheritdoc */
+  getPaths(name) {
+    logger.debug('getPaths', name);
+    return [`${this.path}/${name}-${this.kind}.js`, `${__dirname}/${name}-${this.kind}.js`];
+  }
+
+  /** @inheritdoc */
+  resolutionSucceeded(Resolved) {
+    return new Resolved(this.bot.config);
+  }
+}
+
+module.exports = NluResolver;
