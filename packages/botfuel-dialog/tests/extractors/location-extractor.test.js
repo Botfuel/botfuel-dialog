9aa520023df44e28bc9497dc69cb74ce3ddfe8b2
Kevin Depuydt <kevin@botfuel.io>
2018-03-26 11:01:14 +0200

    add test for LocationExtractor

---
+++ packages/botfuel-dialog/tests/extractors/location-extractor.test.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2017 - present, Botfuel (https://www.botfuel.io).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const LocationExtractor = require('../../src/extractors/location-extractor');
+
+describe('LocationExtractor', () => {
+  test('should extract coordinates', async () => {
+    const extractor = new LocationExtractor();
+    const entities = await extractor.compute("I'm in NYC at coordinates 40.741895,-73.989308");
+    expect(entities).toEqual([
+      {
+        dim: 'system:location',
+        body: '40.741895,-73.989308',
+        values: [{ value: { lat: '40.741895', long: '-73.989308' }, type: 'coordinates' }],
+        start: 26,
+        end: 46,
+      },
+    ]);
+  });
+});
