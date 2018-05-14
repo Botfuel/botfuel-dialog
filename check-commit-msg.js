f7a53af9c3528084c0120ac5a4151d577850414e
Michel Blancard <michel@botfuel.io>
2018-05-14 17:19:31 +0200

    docs: Improve error message for bad commit messages

--- check-commit-msg.js
+++ check-commit-msg.js
@@ -8,10 +8,25 @@ const commitMessage = fs.readFileSync(commitMessageFilename, 'utf8');
 const compliance = commitMessage.match(COMMIT_RULE);
 
 if (!compliance) {
-  console.log('The commit message does not comply with the contributing rules!');
-  console.log('Reminder: commits must comply to the following regex:');
+  console.log('You just tried to commit a change with a commit message that does not comply');
+  console.log('with our contributing rules:');
+  console.log('------------------------------');
+  console.log(commitMessage);
+  console.log('------------------------------');
+  console.log();
+  console.log('Reminder: commits must comply with the following regex:');
   console.log(`"${COMMIT_RULE}"`);
   console.log();
-  console.log(commitMessage);
+  console.log('Example:');
+  console.log('------------------------------');
+  console.log('feat: Add the carrier pigeon adapter.');
+  console.log();
+  console.log('Implement an adapter to the carrier pigeon messaging platform.');
+  console.log();
+  console.log('BREAKING CHANGE: Add a new abstract method Adapter.dealWithLostMessages()');
+  console.log('that must be implemented for each adapter.');
+  console.log('------------------------------');
+  console.log();
+  console.log('Please see CONTRIBUTING.md for more details.');
   process.exit(1);
 }
