5dc5dc912098b1b09f8c1564e0bfa770de05f1e6
Benjamin Heinen <benjamin150594@hotmail.fr>
2018-07-23 12:29:46 +0200

    feat: add pt_br in sdk test: add corpus test for fr, es, pt_br test: fix test

--- packages/botfuel-dialog/tests/extractors/boolean-extractor.test.js
+++ packages/botfuel-dialog/tests/extractors/boolean-extractor.test.js
@@ -16,10 +16,10 @@
 
 const BooleanExtractor = require('../../src/extractors/boolean-extractor');
 
-const extractor = new BooleanExtractor({ locale: 'en' });
 
 describe('BooleanExtractor', () => {
-  test('should properly extract', async () => {
+  test('should properly extract in english', async () => {
+    const extractor = new BooleanExtractor({ locale: 'en' });
     const entities = await extractor.compute('I say yes you say no');
     expect(entities).toHaveLength(2);
     expect(entities[0]).toHaveProperty('body');
@@ -36,5 +36,65 @@ describe('BooleanExtractor', () => {
     expect(entities[1].values[0].value).toBe(true);
     expect(entities[1].start).toBe(6);
     expect(entities[1].end).toBe(9);
+  });
+
+  test('should properly extract in portuguse-bresilian', async () => {
+    const extractor = new BooleanExtractor({ locale: 'pt_br' });
+    const entities = await extractor.compute('Eu digo sim você diz não');
+    expect(entities).toHaveLength(2);
+    expect(entities[0]).toHaveProperty('body');
+    expect(entities[1]).toHaveProperty('body');
+
+    expect(entities[0].dim).toBe('system:boolean');
+    expect(entities[0].values[0].type).toBe('boolean');
+    expect(entities[0].values[0].value).toBe(false);
+    expect(entities[0].start).toBe(21);
+    expect(entities[0].end).toBe(24);
+
+    expect(entities[1].dim).toBe('system:boolean');
+    expect(entities[1].values[0].type).toBe('boolean');
+    expect(entities[1].values[0].value).toBe(true);
+    expect(entities[1].start).toBe(8);
+    expect(entities[1].end).toBe(11);
+  });
+
+  test('should properly extract in french', async () => {
+    const extractor = new BooleanExtractor({ locale: 'fr' });
+    const entities = await extractor.compute('Je dis oui tu dis non');
+    expect(entities).toHaveLength(2);
+    expect(entities[0]).toHaveProperty('body');
+    expect(entities[1]).toHaveProperty('body');
+
+    expect(entities[0].dim).toBe('system:boolean');
+    expect(entities[0].values[0].type).toBe('boolean');
+    expect(entities[0].values[0].value).toBe(false);
+    expect(entities[0].start).toBe(18);
+    expect(entities[0].end).toBe(21);
+
+    expect(entities[1].dim).toBe('system:boolean');
+    expect(entities[1].values[0].type).toBe('boolean');
+    expect(entities[1].values[0].value).toBe(true);
+    expect(entities[1].start).toBe(7);
+    expect(entities[1].end).toBe(10);
+  });
+
+  test('should properly extract in spanish', async () => {
+    const extractor = new BooleanExtractor({ locale: 'es' });
+    const entities = await extractor.compute('Yo digo sí, dices que no');
+    expect(entities).toHaveLength(2);
+    expect(entities[0]).toHaveProperty('body');
+    expect(entities[1]).toHaveProperty('body');
+
+    expect(entities[0].dim).toBe('system:boolean');
+    expect(entities[0].values[0].type).toBe('boolean');
+    expect(entities[0].values[0].value).toBe(false);
+    expect(entities[0].start).toBe(22);
+    expect(entities[0].end).toBe(24);
+
+    expect(entities[1].dim).toBe('system:boolean');
+    expect(entities[1].values[0].type).toBe('boolean');
+    expect(entities[1].values[0].value).toBe(true);
+    expect(entities[1].start).toBe(8);
+    expect(entities[1].end).toBe(10);
   });
 });
