30009dd2d57a9d90d78010b7ed08fa976a94a2fd
KevinDepuydt <kevin.dpdt@gmail.com>
2018-09-13 12:02:11 +0200

    chore: added a failing test on test-complexdialogs package

--- packages/test-complexdialogs/tests/travel.test.js
+++ packages/test-complexdialogs/tests/travel.test.js
@@ -139,4 +139,34 @@ describe('TravelDialog', () => {
     expect(lastConversation.travel._entities.city.values[0].value).toBe('Paris');
     expect(lastConversation.travel._entities.time.body).toBe('the day after tomorrow');
   });
+
+  test('should trigger default dialog when same dialog is triggered twice without entities', async () => {
+    const bot = new Bot(config);
+    const { adapter } = bot;
+    const { userId } = adapter;
+    await bot.play([
+      new UserTextMessage('I leave from Paris'),
+      new UserTextMessage('I am leaving'),
+    ]);
+    expect(bot.adapter.log).toEqual(
+      [
+        new UserTextMessage('I leave from Paris'),
+        new BotTextMessage('Entities defined: city'),
+        new BotTextMessage('Entities needed: time'),
+        new BotTextMessage('Which time?'),
+        new UserTextMessage('I am leaving'),
+        new BotTextMessage('Not understood.'),
+      ].map(msg => msg.toJson(userId)),
+    );
+    const user = await bot.brain.getUser(userId);
+    const dialogs = await bot.brain.getDialogs(userId);
+    const lastConversation = await bot.brain.fetchLastConversation(userId);
+    expect(user._conversations.length).toBe(1);
+    expect(dialogs.stack).toHaveLength(1);
+    expect(dialogs.previous).toHaveLength(1);
+    expect(lastConversation).toHaveProperty('travel');
+    expect(lastConversation.travel._entities).toHaveProperty('city');
+    expect(lastConversation.travel._entities.city).toHaveProperty('body');
+    expect(lastConversation.travel._entities.city.values[0].value).toBe('Paris');
+  });
 });
